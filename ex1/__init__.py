050ec8f8501c7e3fbf6db6d1f1e010254608cdef
Eugene Anfalov <anfalovd10@yandex.ru>
2022-02-01 21:47:32 +0500

    Ex. 1 complete

--- ex1/__init__.py
+++ ex1/__init__.py
@@ -3,5 +3,19 @@ def build_roles_tree(mapping):
     :param mapping: маппинг ролей в категории
     :return: дерево ролей
     """
-    # put your code here
-    pass
+    return {
+        "categories": [
+            {
+                "id": "category-" + mapping["categories"][category_id]["id"],
+                "text": mapping["categories"][category_id]["name"],
+                "items": [
+                    {
+                        "id": mapping["roles"][role_id]["id"],
+                        "text": mapping["roles"][role_id]["name"]
+                    }
+                    for role_id in mapping["categories"][category_id]["roleIds"]
+                ]
+            }
+            for category_id in mapping["categoryIdsSorted"]
+        ]
+    }
